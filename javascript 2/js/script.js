68dbe073459d4670782129a57cd4668b20e55f2d
SnobeS <miko.lehtla@gmail.com>
2023-03-10 10:42:58 +0200

    1.11
    lisätty variableja ja alert

--- javascript 2/js/script.js
+++ javascript 2/js/script.js
@@ -1,6 +1,14 @@
-/*  */
+/* variablet */
 
-var emt = "Variable 1";
+var nimi = "Miko";
+var suku = " Lehtola, ";
+var ikä = 16;
+var opis = " opiskelija";
+
+alert(nimi+suku+ikä+"-vuotias"+opis)
+
+
+
 var test = document.getElementById("otsikko1");
 var oldText = test.innerHTML;
 test.innerHTML = "Moi Tämä On Uusi Otsikko";
